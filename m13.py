4f068491c6a860728c8bdc7b9cfe56b76a933ab6
Jeremy Tregunna <jeremy@tregunna.ca>
2013-03-23 14:56:16 -0600

    target info

--- m13.py
+++ m13.py
@@ -6,26 +6,73 @@ import optparse
 import threading
 import json
 import bottle
-from bottle import Bottle, route, run
+from bottle import Bottle, route, run, request, response
 
 app = Bottle()
 
-def debugger_info():
+def debugger_info(debugger):
+    targets = []
+    for target in debugger:
+        targets.append(info_for_target(target))
     return {
-        'version':lldb.debugger.GetVersionString(),
-        'name':lldb.debugger.GetInstanceName(),
-        'prompt':lldb.debugger.GetPrompt(),
-        'num_targets':lldb.debugger.GetNumTargets()
+        'id':          debugger.GetID(),    
+        'version':     debugger.GetVersionString(),
+        'name':        debugger.GetInstanceName(),
+        'prompt':      debugger.GetPrompt(),
+        'valid':       debugger.IsValid(),
+        'async':       debugger.GetAsync(),
+        'targets':     targets
     }
 
-@app.route('/info')
+@app.get('/debugger')
 def info():
-    return json.dumps(debugger_info())
+    response.content_type = 'application/json; charset=utf8'
+    return json.dumps(debugger_info(lldb.debugger))
 
-@app.route('/debugger/prompt/:prompt')
-def set_prompt(prompt):
+@app.get('/debugger/:debugger_id')
+def debugger_with_id(debugger_id):
+    response.content_type = 'application/json; charset=utf8'
+    debugger = lldb.debugger.FindDebuggerWithID(int(debugger_id))
+    return json.dumps(debugger_info(debugger))
+
+@app.put('/debugger/prompt')
+def set_prompt():
+    response.content_type = 'application/json; charset=utf8'
+    prompt = request.forms.get('prompt')
     lldb.debugger.SetPrompt(prompt)
-    return json.dumps(debugger_info())
+    return json.dumps(debugger_info(lldb.debugger))
+
+def info_for_target(target):
+    breakpoints = []
+    for b in target.breakpoint_iter():
+        breakpoints.append(b.GetID())
+    modules = []
+    for m in target.module_iter():
+        modules.append(m.GetUUIDString())
+    info = {
+        'valid':       target.IsValid(),
+        'debugger_id': target.GetDebugger().GetID(),
+        'exe':         target.GetExecutable().GetFilename(),
+        'exe_path':    target.GetExecutable().GetDirectory() + '/' + target.GetExecutable().GetFilename(),
+        'breakpoints': breakpoints,
+        'modules':     modules
+    }
+    return info
+
+def info_for_target_at_index(idx):
+    target = lldb.debugger.GetTargetAtIndex(idx)
+    return info_for_target(target)
+
+@app.get('/target/selected')
+def selected_target_info():
+    response.content_type = 'application/json; charset=utf8'
+    target = lldb.debugger.GetSelectedTarget()
+    return json.dumps(info_for_target(target))
+
+@app.get('/target/info/:idx')
+def target_info(idx):
+    response.content_type = 'application/json; charset=utf8'
+    return json.dumps(info_for_target(idx))
 
 def m13(debugger, command, result, internal_dict):
     result.PrintCString("Unimplemented")
